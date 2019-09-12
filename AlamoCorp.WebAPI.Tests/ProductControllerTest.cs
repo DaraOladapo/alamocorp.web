160f0c7d65d7af22cf37dd2e47c54fc88b31a330
Dara Oladapo <daraoladapo@outlook.com>
2019-09-12 12:28:24 +0100

    ++test

---
+++ AlamoCorp.WebAPI.Tests/ProductControllerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AlamoCorp.WebAPI.Tests
+{
+    public class ProductControllerTest
+    {
+        [Fact]
+        public void GetProducts()
+        {
+            
+        }
+    }
+}
