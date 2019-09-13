3c4a6dcf793ff507d5d3afeaa90c19d64bf312a0
Dara Oladapo <daraoladapo@outlook.com>
2019-09-13 10:00:00 +0100

    pta

--- AlamoCorp.Web.API/Program.cs
+++ AlamoCorp.Web.API/Program.cs
@@ -19,6 +19,7 @@ namespace AlamoCorp.Web.API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
